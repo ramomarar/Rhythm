7922e918a493258cf94542649dd5f52c91a209ff
Mason Foreman <104833724+MForemxn@users.noreply.github.com>
2025-05-02 13:58:49 +1000

    Fixing File Structure

---
+++ Rhythm App/RhythmTests/RhythmTests.swift
@@ -0,0 +1,16 @@
+//
+//  RhythmTests.swift
+//  RhythmTests
+//
+//  Created by Mason Foreman on 2/5/2025.
+//
+
+import Testing
+
+struct RhythmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
