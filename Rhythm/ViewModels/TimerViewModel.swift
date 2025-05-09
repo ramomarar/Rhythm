d33f1dd74bbc324ccb19c6f89bd501b51ad87edb
clowiclowi <chloewilsonaxl@gmail.com>
2025-05-09 14:49:13 +1000

    Fixing TimerViewModel

--- Rhythm/ViewModels/TimerViewModel.swift
+++ Rhythm/ViewModels/TimerViewModel.swift
@@ -8,36 +8,50 @@ import Foundation
 import SwiftUI
 import Combine
 
-private var scheduler = PomodoroScheduler()
+
 
 class TimerViewModel: ObservableObject {
-    @Published var timeRemaining: Int = 1500
+    @Published var timeRemaining: Int
     @Published var isTimerActive: Bool = false
     @Published var sessionsCompleted: Int = 0
     @Published var currentStreak: Int = 0
-    
+    @Published var currentSession: Session
+
     private var timer: Timer?
-    private var totalTime: Int = 1500
+    private var scheduler = PomodoroScheduler()
     
     var progress: CGFloat {
-        return 1 - CGFloat(timeRemaining) / CGFloat(totalTime)
+        return 1 - CGFloat(timeRemaining) / CGFloat(currentSession.duration)
     }
-    
+
     var formattedTime: String {
         let minutes = timeRemaining / 60
         let seconds = timeRemaining % 60
         return String(format: "%02d:%02d", minutes, seconds)
     }
-    
+
     var stateTitle: String {
-        isTimerActive ? "Focus Time" : "Paused"
+        if isTimerActive {
+            switch currentSession.type {
+            case .focus: return "Focus Time"
+            case .shortBreak: return "Short Break"
+            case .longBreak: return "Long Break"
+            }
+        } else {
+            return "Paused"
+        }
     }
-    
-    
+
+    init() {
+        let session = scheduler.nextSession()
+        self.currentSession = session
+        self.timeRemaining = Int(session.duration)
+    }
+
     func startTimer() {
         guard !isTimerActive else { return }
         isTimerActive = true
-        
+
         timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
             if self.timeRemaining > 0 {
                 self.timeRemaining -= 1
@@ -47,30 +61,32 @@ class TimerViewModel: ObservableObject {
         }
     }
 
-    
     func pauseTimer() {
         isTimerActive = false
         timer?.invalidate()
     }
-    
+
     func resetTimer() {
-            pauseTimer()
-            timeRemaining = totalTime
-        }
-
-        func skipToNext() {
-            completeSession()
-        }
-
-        private func completeSession() {
-            pauseTimer()
-            sessionsCompleted += 1
-            currentStreak += 1
-            resetTimer()
-        }
-
-        func handleForegroundTransition() {}
-        func handleBackgroundTransition() {
-            pauseTimer()
-        }
+        pauseTimer()
+        timeRemaining = Int(currentSession.duration)
     }
+
+    func skipToNext() {
+        completeSession()
+    }
+
+    private func completeSession() {
+        pauseTimer()
+        sessionsCompleted += 1
+        currentStreak += 1
+
+        let next = scheduler.nextSession()
+        currentSession = next
+        timeRemaining = Int(next.duration)
+    }
+
+    func handleForegroundTransition() {}
+    func handleBackgroundTransition() {
+        pauseTimer()
+    }
+}
