a05228923d50eaebd995aa48eae9c89dde9efbbe
clowiclowi <chloewilsonaxl@gmail.com>
2025-05-09 14:29:08 +1000

    Initialised the PodomoroScheduler and the configurable durations

--- Rhythm/Services/PomodoroScheduler.swift
+++ Rhythm/Services/PomodoroScheduler.swift
@@ -5,5 +5,18 @@
 //  Created by Mason Foreman on 2/5/2025.
 //
 
+import Foundation
 
+class PomodoroScheduler {
+    private(set) var completedFocusSessions = 0
+    
+        //the configurablel session durations (in seconds so 25 * 60 is 25 mins etc )
+    let focusDuration: TimeInterval = 25 * 60
+       let shortBreakDuration: TimeInterval = 5 * 60
+       let longBreakDuration: TimeInterval = 15 * 60
+       let longBreakInterval = 4
+    
+    private(set) var currentType: SessionType = .focus
+    
+}
 
